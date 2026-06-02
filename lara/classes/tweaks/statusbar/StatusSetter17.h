98ee9ddc186737263ba321577a19f7f36ae2b07b
Yupa <yupayupa0625@icloud.com>
2026-06-02 22:44:39 +0900

    Update StatusSetter17.h

--- lara/classes/tweaks/statusbar/StatusSetter17.h
+++ lara/classes/tweaks/statusbar/StatusSetter17.h
@@ -3,14 +3,22 @@
 //  lara
 //
 //  Based on StatusSetter16_1 from Cowabunga (MIT License)
-//  Adapted for iOS 17+ (lara targets iOS 17.0 - 18.7.1, iOS 26.0.x)
-//
-//  NOTE: StatusBarOverrideData struct may differ across iOS versions.
-//        Verify sizeof(StatusBarOverrideData) matches SpringBoard's symbol on each target version.
+//  Adapted for lara: 書き込み/読み込みを関数ポインタで外部から注入する
 //
 
 #pragma once
 #import "StatusSetter.h"
 
+// StatusSetter17 が使う書き込み/読み込み関数の型
+typedef BOOL  (*StatusBarWriteFunc)(const void *data, size_t len);
+typedef BOOL  (*StatusBarReadFunc)(void *buf, size_t len);
+typedef BOOL  (*StatusBarExistsFunc)(void);
+
 @interface StatusSetter17 : NSObject <StatusSetter>
+
+// lara の Swift 側から初期化時に注入する
++ (void)setWriteFunc:(StatusBarWriteFunc)writeFunc
+           readFunc:(StatusBarReadFunc)readFunc
+         existsFunc:(StatusBarExistsFunc)existsFunc;
+
 @end
