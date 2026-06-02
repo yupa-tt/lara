25f3160e731fdcb5dbc30d535c0d1e97705eb20d
Yupa <yupayupa0625@icloud.com>
2026-06-02 22:57:00 +0900

    Update StatusBarSetup.swift

--- lara/classes/tweaks/statusbar/StatusBarSetup.swift
+++ lara/classes/tweaks/statusbar/StatusBarSetup.swift
@@ -10,14 +10,14 @@ private let kOverridesPath = "/var/mobile/Library/SpringBoard/statusBarOverrides
 func setupStatusBar() {
     StatusSetter17.setWriteBlock({ ptr, len in
         guard let ptr = ptr else { return false }
-        let data = Data(bytes: ptr, count: len)
+        let data = Data(bytes: ptr, count: Int(len))
         let result = laramgr.shared.lara_overwritefile(target: kOverridesPath, data: data)
         return result.ok
 
     }, readBlock: { ptr, len in
         guard let ptr = ptr else { return false }
-        guard let data = laramgr.shared.vfsread(path: kOverridesPath, maxSize: len) else { return false }
-        let copyLen = min(data.count, len)
+        guard let data = laramgr.shared.vfsread(path: kOverridesPath, maxSize: Int(len)) else { return false }
+        let copyLen = min(data.count, Int(len))
         data.withUnsafeBytes { src in
             guard let base = src.baseAddress else { return }
             memcpy(ptr, base, copyLen)
