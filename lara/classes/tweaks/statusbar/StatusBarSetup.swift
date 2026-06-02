627c4e75f2b547a2f88e5335f5b47f88644fa750
Yupa <yupayupa0625@icloud.com>
2026-06-02 23:09:21 +0900

    Update StatusBarSetup.swift

--- lara/classes/tweaks/statusbar/StatusBarSetup.swift
+++ lara/classes/tweaks/statusbar/StatusBarSetup.swift
@@ -8,10 +8,14 @@ import Foundation
 private let kOverridesPath = "/var/mobile/Library/SpringBoard/statusBarOverrides"
 
 func setupStatusBar() {
+    laramgr.shared.logmsg("setupStatusBar: sbxready=\(laramgr.shared.sbxready) vfsready=\(laramgr.shared.vfsready)")
+
     StatusSetter17.setWrite({ ptr, len in
         guard let ptr = ptr else { return false }
         let data = Data(bytes: ptr, count: Int(len))
+        laramgr.shared.logmsg("statusbar write: \(Int(len)) bytes -> \(kOverridesPath)")
         let result = laramgr.shared.lara_overwritefile(target: kOverridesPath, data: data)
+        laramgr.shared.logmsg("statusbar write result: \(result.ok) \(result.message)")
         return result.ok
 
     }, read: { ptr, len in
